006f41fcce74891de86010f6db434840c9aadfb4
mbaayang <mbayangueye07@gmail.com>
2023-03-19 19:21:38 +0000

    page paramétre

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import CtrlSysteme from './components/CtrlSysteme/CtrlSysteme'
 import Header from './components/Header/Header'
 import Mesures from './components/Mesures/Mesures'
+import Parametre from './components/Parametres/Parametre'
 import Sidebar from './components/Sidebar/Sidebar'
 
 function App() {
