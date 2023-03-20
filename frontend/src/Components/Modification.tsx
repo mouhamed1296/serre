7e1b582809c8fb361cd1ca4314e465e0dc4518bf
issa <i.ndiaye@isepdiamniadio.edu.sn>
2023-03-20 18:08:22 +0000

    debut

--- frontend/src/Components/Modification.tsx
+++ frontend/src/Components/Modification.tsx
@@ -1,27 +1,37 @@
 import React from "react";
-import './Modification.css'
-
+import "./Modification.css";
+import { useForm } from "react-hook-form";
 
 const Modification = () => {
   const handleFormSubmit = (e: any) => {
     e.preventDefault();
 
-    let email = e.target.elements.email?.value;
-    let password = e.target.elements.password?.value;
+    // let email = e.target.elements.email?.value;
+    // let password = e.target.elements.password?.value;
 
-    console.log(password, password);
+    // console.log(password, password);
   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data: any) => console.log(data);
+  console.log(errors);
+
   return (
     <div className="bg-gray-bg1 flex h-screen">
       <div className="border-primaryBorder shadow-default m-auto w-full max-w-md rounded-lg border bg-white py-10 px-16">
         <h1 className="text-primary mt-4 mb-12 text-center text-2xl font-medium ">
-          Modfier le mot de passe 
+          Modfier le mot de passe
         </h1>
 
-        <form onSubmit={handleFormSubmit}>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div>
             <label htmlFor="password">Actuel mot de passe</label>
             <input
+              {...register("actuelPassword", { required: true, maxLength: 80 })}
               type="password"
               className={`text-primary mb-4 w-full rounded-md border p-2 text-sm outline-none transition duration-150 ease-in-out`}
               id="email"
@@ -31,6 +41,10 @@ const Modification = () => {
           <div>
             <label htmlFor="password">Nouveau mot de passe</label>
             <input
+              {...register("nouveauPassword", {
+                required: true,
+                maxLength: 80,
+              })}
               type="password"
               className={`text-primary mb-4 w-full rounded-md border p-2 text-sm outline-none transition duration-150 ease-in-out`}
               id="password"
@@ -40,6 +54,10 @@ const Modification = () => {
           <div>
             <label htmlFor="password">Confirmation mot de passe</label>
             <input
+              {...register("confirmationPassword", {
+                required: true,
+                maxLength: 80,
+              })}
               type="password"
               className={`text-primary mb-4 w-full rounded-md border p-2 text-sm outline-none transition duration-150 ease-in-out`}
               id="password"
@@ -49,6 +67,7 @@ const Modification = () => {
 
           <div className="mt-6 flex items-center justify-center">
             <button
+              type="submit"
               className={`bg-green border-green focus:border-green-dark rounded border py-2 px-4 text-sm text-black focus:outline-none`}
             >
               Modifier
@@ -61,3 +80,8 @@ const Modification = () => {
 };
 
 export default Modification;
+/* 
+
+Modification
+
+*/
